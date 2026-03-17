6f83d4eb7f3e63346e6240f59c2bd993ebc2879d
vladwb <vladwithcode@gmail.com>
2026-03-17 10:51:49 -0600

    chore: add animations

---
+++ app/animations.ts
@@ -0,0 +1,17 @@
+export const animations: Record<string, GSAPTweenVars> = {
+    fadein: {
+        opacity: 1,
+    },
+    slideX: {
+        opacity: 1,
+        x: "0rem",
+    },
+    slideY: {
+        opacity: 1,
+        y: "0rem",
+    },
+    scale: {
+        opacity: 1,
+        scale: 1,
+    },
+}
